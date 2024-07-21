ffd674d6f555f62917af44670c2a0fce376a7806
siddhant-sri <sidz.dec26@gmail.com>
2024-07-21 15:48:55 +0530

    required validation on form

--- src/components/custom/ProjectDiscussion.js
+++ src/components/custom/ProjectDiscussion.js
@@ -24,52 +24,66 @@ const ProjectDiscussion = () => {
         <div className="w-full md:w-1/2 mt-10 md:mt-[5%]">
           <div className="w-full md:w-5/6 shadow-xl">
             <Card>
-              <div className="flex flex-col md:flex-row p-6">
-                <div className="flex flex-col p-4">
-                  <label htmlFor="full-name">Full name</label>
-                  <input
-                    className="mb-4 outline-none border-gray-200 border-b-2"
-                    type="text"
-                    required
-                    placeholder="Enter Name"
-                    onChange={(e) => e.target.value}
-                  />
-                  <label htmlFor="biz-name">Business name</label>
-                  <input
-                    type="text"
-                    placeholder="Enter Business Name"
-                    required
-                    className="outline-none border-gray-200 border-b-2"
-                    onChange={(e) => e.target.value}
-                  />
+              <form>
+                <div className="flex flex-col md:flex-row p-6">
+                  <div className="flex flex-col p-4">
+                    <label htmlFor="full-name">
+                      {" "}
+                      <span className="text-red-600">*</span> Full name
+                    </label>
+                    <input
+                      className="mb-4 outline-none border-gray-200 border-b-2"
+                      type="text"
+                      required
+                      placeholder="Enter Name"
+                      onChange={(e) => e.target.value}
+                    />
+                    <label htmlFor="biz-name">
+                      {" "}
+                      <span className="text-red-600">*</span> Business name
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Enter Business Name"
+                      required
+                      className="outline-none border-gray-200 border-b-2"
+                      onChange={(e) => e.target.value}
+                    />
+                  </div>
+                  <div className="flex flex-col p-4">
+                    <label htmlFor="phone">
+                      {" "}
+                      <span className="text-red-600">*</span> Phone number
+                    </label>
+                    <input
+                      className="mb-4 outline-none border-gray-200 border-b-2"
+                      type="number"
+                      required
+                      placeholder="Enter Number"
+                      onChange={(e) => e.target.value}
+                    />
+                    <label htmlFor="biz-mail">
+                      {" "}
+                      <span className="text-red-600">*</span> Business mail
+                    </label>
+                    <input
+                      type="email"
+                      required
+                      className="outline-none border-gray-200 border-b-2"
+                      placeholder="Enter mail"
+                      onChange={(e) => e.target.value}
+                    />
+                  </div>
                 </div>
-                <div className="flex flex-col p-4">
-                  <label htmlFor="phone">Phone number</label>
-                  <input
-                    className="mb-4 outline-none border-gray-200 border-b-2"
-                    type="number"
-                    required
-                    placeholder="Enter Number"
-                    onChange={(e) => e.target.value}
-                  />
-                  <label htmlFor="biz-mail">Business mail</label>
-                  <input
-                    type="email"
-                    required
-                    className="outline-none border-gray-200 border-b-2"
-                    placeholder="Enter mail"
-                    onChange={(e) => e.target.value}
-                  />
+                <div className="flex container justify-end mb-5">
+                  <Button
+                    className="text-white bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2"
+                    type="submit"
+                  >
+                    Discuss the project
+                  </Button>
                 </div>
-              </div>
-              <div className="flex container justify-end mb-5">
-                <Button
-                  className="text-white bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2"
-                  type="submit"
-                >
-                  Discuss the project
-                </Button>
-              </div>
+              </form>
             </Card>
           </div>
         </div>
